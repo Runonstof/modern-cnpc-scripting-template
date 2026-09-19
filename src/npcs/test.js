8c18cea83bfd3c3ae31841191a2f1628c5c5047a
Ronan <36049776+Runonstof@users.noreply.github.com>
2026-09-19 13:09:51 +0200

    Cleanup non-1.12.2 scripts

--- src/npcs/test.js
+++
@@ -1,6 +0,0 @@
-import { getFieldNames } from "~/lib/reflection";
-
-export function interact(e) {
-    // e.npc.say(getFieldNames(e.player.getMCEntity()).join('\n'));
-    e.npc.say(e.player.getMCEntity().m_20194_ + '');
-}
